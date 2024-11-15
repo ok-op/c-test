eac154b096e9388fcf0d77e6e5d64d55c9907fef
jaan op <125698743+ok-op@users.noreply.github.com>
2024-11-15 07:01:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="inst_loader",
     version="0.1",
-    packages=find_packages(),
+    py_modules=["inst_loader"],
     install_requires=[],
 )
