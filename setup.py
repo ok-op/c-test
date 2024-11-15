eff1d61790c8f5fa8f5cb153a8c7487c5e4415da
jaan op <125698743+ok-op@users.noreply.github.com>
2024-11-15 08:49:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-# setup.py
 from setuptools import setup
 
 setup(
-    name="inst_loader",
-    version="0.1",
-    py_modules=["inst_loader"],  # যদি আপনার ফাইল inst_loader.py হয়, তাহলে এটিই ব্যবহার করুন
-    install_requires=[],  # অন্য কোনো ডিপেনডেন্সি থাকলে তা এখানে লিখুন
+    name='inst_loader',  # প্যাকেজের নাম
+    version='0.1',
+    py_modules=['inst_loader'],
+    install_requires=[
+        'requests',  # আপনার অন্য ডিপেনডেন্সি
+        'Flask',
+    ],
 )
